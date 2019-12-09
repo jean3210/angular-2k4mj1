036cef75f3b4df64ea5e955be773e45f35a9e598
jean3210 <dad.muti@gmail.com>
2019-12-09 12:10:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-2k4mj1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-2k4mj1)
---
+++ src/app/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  templateUrl: `./hello.component.html`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class HelloComponent  {
+  @Input() name: string;
+  @Input() text: string = "tessttttt";
+}
